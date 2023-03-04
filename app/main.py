6a8568229b2d1d866f789d753a6ba0fe2f4215e0
Luis Rojas <lumrojasmu@unal.edu.co>
2023-03-03 20:07:06 -0500

    Mi primer cambio

--- app/main.py
+++ app/main.py
@@ -4,7 +4,7 @@ from api.routes.api import router as api_router
 from core.events import create_start_app_handler
 from core.config import API_PREFIX, DEBUG, PROJECT_NAME, VERSION
 
-
+//luis rojas
 def get_application() -> FastAPI:
     application = FastAPI(title=PROJECT_NAME, debug=DEBUG, version=VERSION)
     application.include_router(api_router, prefix=API_PREFIX)
