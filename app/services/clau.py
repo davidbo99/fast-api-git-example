f332a1b831a5b44a17c957e64f03bd0e1d03aa3d
Claudia Arciniegas <claudia.arciniegas@outlook.com>
2023-03-03 20:48:31 -0500

    cambios en clau.py

--- app/services/clau.py
+++ app/services/clau.py
@@ -15,3 +15,21 @@ class MachineLearningModelHandlerScore(object):
         if hasattr(clf, method):
             return getattr(clf, method)(input)
         raise PredictException(f"'{method}' attribute is missing")
+
+        import os
+
+from loguru import logger
+
+from core.errors import PredictException, ModelLoadException
+from core.config import MODEL_NAME, MODEL_PATH
+
+
+class MachineLearningModelHandlerScore(object):
+    model = None
+
+    @classmethod
+    def predict(cls, input, load_wrapper=None, method="predict"):
+        clf = cls.get_model(load_wrapper)
+        if hasattr(clf, method):
+            return getattr(clf, method)(input)
+        raise PredictException(f"'{method}' attribute is missing")
