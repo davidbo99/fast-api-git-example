4150cb414849a94483c408ea765f0ae5436c43bb
Oscar Mora Bonnett <oscarmorabonnett@gmail.com>
2023-03-03 20:24:49 -0500

    app/mai.py

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
+//oscar mora bonnett
 
-from api.routes.api import router as api_router
 from core.events import create_start_app_handler
 from core.config import API_PREFIX, DEBUG, PROJECT_NAME, VERSION
 
