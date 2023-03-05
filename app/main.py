a98d36b2e1c48eec6a5114914f5539f57dc17181
FRANCIA JARAMILLO <francia23milena@hotmail.com>
2023-03-05 18:11:45 -0500

    se agrego primer cambio en main.py

--- app/main.py
+++ app/main.py
@@ -8,7 +8,7 @@ from fastapi import FastAPI
 from core.events import create_start_app_handler
 from core.config import API_PREFIX, DEBUG, PROJECT_NAME, VERSION
 
-//francia paso por aqui por seguna vez
+//francia paso por aqui por seguna vez primer cambio
 def get_application() -> FastAPI:
     application = FastAPI(title=PROJECT_NAME, debug=DEBUG, version=VERSION)
     application.include_router(api_router, prefix=API_PREFIX)
