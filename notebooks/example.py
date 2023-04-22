c34797bb9803a46565bc71d960db0bf556cc4690
German Moran <homerogpm@gmail.com>
2023-04-22 10:20:23 -0500

    Agregar operacion division  al archivo example.py

--- notebooks/example.py
+++ notebooks/example.py
@@ -1,14 +1,18 @@
-# codigo para sumar 2 numero
+# 1. codigo para sumar 2 numero
 num1 = 2
 num2 = 5
 suma = num1+num2
 print("La suma de los 2 numeros es: ",suma)
 
-#Agrego los segundos cambios
+# 2. Agrego los segundos cambios
 reta = num1 -num2
 print("La resta de los 2 numeros es: ", resta)
 
-# Agrego los terceros cambios
+#3. Agrego los terceros cambios
 multi = num1*num2
 print("La multiplecacion de los 2 numeros es:", num1*num2)
+
+#4. Agrego el cuarto cambio
+div = num1/num2
+print("La division de los 2 numeros es:", div)
 
