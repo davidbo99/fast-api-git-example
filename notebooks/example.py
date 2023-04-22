4fd422ffeb23b027b25461bcd141e99a3e729429
German Moran <homerogpm@gmail.com>
2023-04-22 10:17:47 -0500

    Agregar operacion multiplicacion  al archivo example.py

--- notebooks/example.py
+++ notebooks/example.py
@@ -7,3 +7,8 @@ print("La suma de los 2 numeros es: ",suma)
 #Agrego los segundos cambios
 reta = num1 -num2
 print("La resta de los 2 numeros es: ", resta)
+
+# Agrego los terceros cambios
+multi = num1*num2
+print("La multiplecacion de los 2 numeros es:", num1*num2)
+
