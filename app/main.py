a86c8d8a1ab26ecfff080809305fc0b1b0a62b45
Daniel Colorado <dperez0194@gmail.com>
2023-08-15 18:04:37 -0500

    Se cambian comentarios en main.py y archivo init.php se corrigen los comentarios

--- app/main.py
+++ app/main.py
@@ -1,5 +1,4 @@
-from fastapi import FastAPI
-
+#from fastapi import FastAPI
 from api.routes.api import router as api_router
 from core.events import create_start_app_handler
 from core.config import API_PREFIX, DEBUG, PROJECT_NAME, VERSION
